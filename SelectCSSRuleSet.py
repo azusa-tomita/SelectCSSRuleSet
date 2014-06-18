a817d59c33ee77774087cbc7b343e0bf7f0b2873
azusa-tomita <martinsvogel@gmail.com>
2014-06-18 17:46:28 +0900

    細かいバグ修正
    セレクタ以外の場合は直近の}にカーソル移動したほうがラクな気がしてきた

--- SelectCSSRuleSet.py
+++ SelectCSSRuleSet.py
@@ -5,180 +5,202 @@ class select_css_rule_set(sublime_plugin.TextCommand):
 	def run(self, edit):
 		view = self.view
 
-		def findStr(key, point = view.sel()[0].begin(),dir="f"):
+		# find except the comment of CSS and SCSS
+		def findStr(key, point = view.sel()[0].begin(), dir = "f"):
 			keyLength = len(key)
-			res = val = lineVal = ""
-			isCSSComment = isSassComment = False
-			i = j = sass = cssOp = cssCl =  0
-			beforeLine = view.split_by_newlines ( sublime.Region(0,point) )
-			currentLine = len(beforeLine) - 1
+			target = val = lineVal = ""
+			cmtSCSSPos = cmtCSSPosS = cmtCSSPosE =  -1
+			isCmtCSS = isCmtSCSS = False
+			i = j = 0
+			bfLines = view.split_by_newlines(sublime.Region(0, point + 1))
+			currLineNum = len(bfLines) - 1
 
-			if view.substr( beforeLine[currentLine] ).find("//") > 0:
-				isSassComment = True
-				sass = view.substr( beforeLine[currentLine] ).find("//")
+			if view.substr(bfLines[currLineNum]).find("//") >= 0:
+				isCmtSCSS = True
+				cmtSCSSPos = view.substr(bfLines[currLineNum]).find("//")
 
-			j = currentLine
+			j = currLineNum
 			while j >= 0:
-				val = view.substr( beforeLine[j] )
-				if cssOp and cssCl:
+				val = view.substr(bfLines[j])
+				if cmtCSSPosS != -1 and cmtCSSPosE != -1:
 					break
 				else:
-					if cssOp == 0 and val.rfind("/*") != -1:
-						cssOp = beforeLine[j].begin() + val.rfind("/*")
-					if cssCl == 0 and val.rfind("*/") != -1:
-						cssCl = beforeLine[j].begin() + val.rfind("*/") + 1
+					_s = val.rfind("/*")
+					_e = val.rfind("*/")
+					if cmtCSSPosS == -1 and _s != -1:
+						cmtCSSPosS = bfLines[j].begin() + _s
+					if cmtCSSPosE == -1 and _e != -1:
+						cmtCSSPosE = bfLines[j].begin() + _e + 1
 					j = j - 1
-
-			if cssCl < cssOp:
-				isCSSComment = True
+			if cmtCSSPosE < cmtCSSPosS:
+				isCmtCSS = True
 
 			if dir == "f":
 				while view.size() - (point + i) != 0:
-					val = view.substr( sublime.Region( point + i,point + i + keyLength) )
+					val = view.substr(sublime.Region(point + i, point + i + keyLength))
 
 					if val[0] == "/":
 						if view.substr(point + i + 1) == "*":
-							isCSSComment = True
+							isCmtCSS = True
 						if view.substr(point + i - 1) == "*":
-							isCSSComment = False
+							isCmtCSS = False
 
 					if val[0] == "/":
 						if view.substr(point + i + 1) == "/":
-							isSassComment = True
+							isCmtSCSS = True
 
 					if val[0] == "\n":
-						if isSassComment == True:
-							isSassComment = False
+						if isCmtSCSS == True:
+							isCmtSCSS = False
 
-					if isCSSComment == False and isSassComment == False:
+					if isCmtCSS == False and isCmtSCSS == False:
 						if key == val:
-							res = sublime.Region( point + i,point + i + keyLength)
+							target = sublime.Region(point + i, point + i + keyLength)
 							break
 					i = i + 1
 				else:
-					res = sublime.Region( -1,-1)
+					target = sublime.Region(-1, -1)
 
 			else:
-				j = currentLine
+				j = currLineNum
 				while j >= 0:
-					lineValS = beforeLine[j].begin()
-					lineValE = beforeLine[j].end()-1
-					lineVal = view.substr( beforeLine[j] )
+					lineValS = bfLines[j].begin()
+					lineValE = bfLines[j].end() - 1
+					lineVal = view.substr(bfLines[j])
 
-					if j == currentLine:
-						lineVal = view.substr( sublime.Region(beforeLine[j].begin(),point) )
-
-						if isSassComment == True:
-							lineVal = view.substr( sublime.Region(lineValS,lineValS + sass) )
+					if j == currLineNum:
+						if isCmtSCSS == True:
+							lineVal = view.substr(sublime.Region(lineValS, lineValS + cmtSCSSPos))
+						else:
+							lineVal = view.substr(sublime.Region(bfLines[j].begin(), point))
 					else:
-						sass = lineVal.find("//")
-						if sass != -1:
-							lineVal = view.substr( sublime.Region(lineValS,lineValS + sass) )
+						cmtSCSSPos = lineVal.find("//")
+						if cmtSCSSPos != -1:
+							lineVal = view.substr(sublime.Region(lineValS, lineValS + cmtSCSSPos))
 
 					i = 0
-					while len(lineVal)-keyLength >= i:
-						valE = len(lineVal)-i
+					while len(lineVal) - keyLength >= i:
+						valE = len(lineVal) - i
 						valS = valE - keyLength
 						val = lineVal[valS:valE]
 
 						sl = val.rfind("/")
 						if sl != -1:
-							if view.substr(lineValS + valS + sl -1) == "*":
-								isCSSComment = True
-							if view.substr(lineValS + valS + sl +1) == "*":
-								isCSSComment = False
+							if view.substr(lineValS + valS + sl - 1) == "*":
+								isCmtCSS = True
+							if view.substr(lineValS + valS + sl + 1) == "*":
+								isCmtCSS = False
 
-						if isCSSComment == False and isSassComment == False:
+						if isCmtCSS == False and isCmtSCSS == False:
 							if key == val:
-								res = sublime.Region( lineValS + valS,lineValS + valE)
+								target = sublime.Region(lineValS + valS, lineValS + valE)
 								break
 
 						i = i + 1
 
-					if res !="":
+					if target != "":
 						break
 					j = j - 1
 				else:
-					res = sublime.Region( -1,-1)
-			return res
+					target = sublime.Region(-1, -1)
+			return target
+
 
 		sel = view.sel()[0]
-		nearestOpCurly = findStr('{',point = sel.begin()).begin()
-		nearestClCurly = findStr('}',point = sel.begin()).begin()
-		nearestSColon  = findStr(';',point = sel.begin()).begin()
-		if view.substr( sel.begin() - 1 ) == ";" and view.substr( sel.begin()) == "\n":
-			nearestSColon  = findStr(';',point = sel.begin(),dir="r").begin()
+		selS = sel.begin()
+
+		# Adjustment of the cursor position for expand_selection
+		nearestLCurly = findStr('{', point = selS).begin()
+		nearestRCurly = findStr('}', point = selS).begin()
+		nearestSColon = findStr(';', point = selS).begin()
+		if view.substr(selS - 1) == ";" and view.substr(selS) == "\n":
+			nearestSColon  = findStr(';', point = selS, dir = "r").begin()
 
 		isSelector = False
-		if nearestOpCurly < 0:
-			if nearestClCurly < 0:
+		if nearestLCurly < 0:
+			if nearestRCurly < 0:
 				isSelector = True
 			else:
 				isSelector = False
 		else:
-			if nearestClCurly < 0:
+			if nearestRCurly < 0:
 				isSelector = False
 			else:
-				if nearestClCurly < nearestOpCurly:
+				if nearestRCurly < nearestLCurly:
 					isSelector = False
 				else:
 					if nearestSColon < 0:
 						isSelector = True
 					else:
-						if nearestOpCurly < nearestSColon:
+						if nearestLCurly < nearestSColon:
 							isSelector = True
 						else:
 							isSelector = False
 
+		## if selector
 		if isSelector :
 			view.sel().clear()
-			view.sel().add(nearestOpCurly)
+			view.sel().add(nearestLCurly)
+			print("1")
 
-		if (view.substr( sel.begin() - 1 ) == "}" or view.substr( sel.begin() - 1 ) == "{") and view.substr( sel.begin()) == "\n":
+		## If there is a cursor just after {} and the back of the cursor was a newline
+		if ((view.substr(selS - 1) == "}" or view.substr(selS - 1) == "{" ) and
+				view.substr(selS) == "\n"):
 			view.sel().clear()
-			view.sel().add(sublime.Region(sel.begin()-1,sel.begin()-1))
+			view.sel().add(sublime.Region(selS - 1, selS - 1))
+			print("2")
 
-		if view.substr( sel.begin() ) == "(" or view.substr( sel.begin() - 1 ) == ")" or findStr("(",dir="r") > findStr(")",dir="r"):
+		## If there is a cursor inside of ()
+		if (view.substr(selS) == "(" or                         # hoge:|(foo);
+				view.substr(selS - 1) == ")" or                     # hoge:(foo)|;
+				findStr(")", dir = "r") < findStr("(", dir = "r")   # hoge:(fo|o);
+				):
 			view.sel().clear()
-			view.sel().add(max(findStr("{",dir="r").begin(),findStr("}",dir="r").begin()))
+			view.sel().add(findStr(":", dir = "r").begin())
+			print("3")
 
-		if nearestOpCurly < 0 and nearestClCurly < 0:
+		## If there is not a rule set after a cursor
+		if nearestLCurly < 0 and nearestRCurly < 0:
 			view.sel().clear()
-			view.sel().add(findStr("}",dir="r"))
+			view.sel().add(findStr("}", dir = "r"))
+			print("4")
 
+
+		# The acquisition of css block
 		view.run_command('expand_selection', {'to': 'brackets'})
 		view.run_command('expand_selection', {'to': 'brackets'})
 		cssBlock = view.sel()[0]
 
 
-		fNearestOpCurly = findStr('{',point = cssBlock.begin()-1,dir="r").end() 
-		fNearestClCurly = findStr('}',point = cssBlock.begin()-1,dir="r").end()
-		fNearestSColon  = findStr(';',point = cssBlock.begin()-1,dir="r").end()
+		# The acquisition of css rule set
+		ruleSetPosE = cssBlock.end()
 
-		ruleSetOp = max(fNearestOpCurly, fNearestClCurly, fNearestSColon)
+		rNearestLCurly = findStr('{', point = cssBlock.begin() - 1, dir = "r").end() 
+		rNearestRCurly = findStr('}', point = cssBlock.begin() - 1, dir = "r").end()
+		rNearestSColon = findStr(';', point = cssBlock.begin() - 1, dir = "r").end()
+		ruleSetPosS = max(rNearestLCurly, rNearestRCurly, rNearestSColon)
+		if ruleSetPosS == -1:
+			ruleSetPosS = 0
 
-		i = ruleSetOp
-		isCSSComment = isSassComment = False
+		i = ruleSetPosS
+		isCmtCSS = isCmtSCSS = False
 		while i < cssBlock.begin():
-			if isCSSComment == True or isSassComment == True:
-				if isCSSComment == True and view.substr(i) == "*" and view.substr(i + 1) == "/":
-					isCSSComment = False
+			if isCmtCSS == True or isCmtSCSS == True:
+				if isCmtCSS == True and view.substr(i) == "*" and view.substr(i + 1) == "/":
+					isCmtCSS = False
 					i = i + 1
-				elif isSassComment == True and view.substr(i) == "\n":
-					isSassComment = False
+				elif isCmtSCSS == True and view.substr(i) == "\n":
+					isCmtSCSS = False
 			elif view.substr(i) == "/" and view.substr(i + 1) == "*":
-				isCSSComment = True
+				isCmtCSS = True
 			elif view.substr(i) == "/" and view.substr(i + 1) == "/":
-				isSassComment = True
-			elif (view.substr(i) == "\n" or
-						view.substr(i) == "\t" or
-						view.substr(i) == " "
-					):
+				isCmtSCSS = True
+			elif (view.substr(i) == "\n" or view.substr(i) == "\t" or view.substr(i) == " "):
 				pass
 			else:
 				break
 			i = i + 1
-		ruleSetOp = i
+		ruleSetPosS = i
 
 		view.sel().clear()
-		view.sel().add( sublime.Region(ruleSetOp,cssBlock.end()) )
+		view.sel().add(sublime.Region(ruleSetPosS, ruleSetPosE))
